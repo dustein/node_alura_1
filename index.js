a5c1c592c5f514fe85775f906ce00efc985beb73
dustein <steindu@gmail.com>
2022-03-04 11:39:37 -0300

    reiniciando projeto

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
 
-console.log(chalk.green("testando"))
+function extraiLinks(texto) {
+    const regex = "";
+    const arrayResultados = [];
+    let temp;
+    while((temp = regex.exec(texto)) !== null) {
+        arrayResultados.push( {[temp[1]]: temp[2]} )
+    };
+    return arrayResultados.length === 0 ? 'Não há links' : arrayResultados;
+}
+
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, ' -> arquivo não encontrado'));
+}
+
