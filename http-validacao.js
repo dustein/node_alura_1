f98d4a84f6d9d338319b8360c8975f904430b482
DuStein <steindu@gmail.com>
2022-03-05 23:23:37 -0300

    resolver resposta em array

--- http-validacao.js
+++ http-validacao.js
@@ -1,12 +1,22 @@
-import { fetch } from 'node-fetch';
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+async function checkStatus(arrayURLs) {
+    const arrayStatus = await Promise.all(arrayURLs.map(async (url) => {
+        const resp = await fetch(url)
+        return resp.status;
+    }))
+    return arrayStatus
+}
 
 function geraArrayDeURLs(arrayLinks) {
     console.log(arrayLinks.map(objetoLink => Object.values(objetoLink).join()))
     return arrayLinks.map(objetoLink => Object.values(objetoLink).join());
 }
 
-function validaURLs(arrayLinks) {
-    return geraArrayDeURLs(arrayLinks)
+async function validaURLs(arrayLinks) {
+    const links = geraArrayDeURLs(arrayLinks);
+    const statusLinks = await checkStatus(links);
+    return statusLinks;
 };
 
 export { validaURLs };
